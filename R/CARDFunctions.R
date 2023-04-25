1da12b25810e2dc85b0d89b7d8a358b8f5544763
Fernando Sola <fersolesp@alum.us.es>
2023-04-25 11:45:10 +0200

    CARD to UniProt and KEGG implemented

--- R/CARDFunctions.R
+++ R/CARDFunctions.R
@@ -41,7 +41,53 @@ getCARD2NCBIGene <- function(cardId, exhaustiveMapping = FALSE){
     return(result)
 }
 
+############################
+# CARD database to UniProt #
+############################
 
+getCARD2UniProt <- function(cardId, exhaustiveMapping = FALSE){
+
+    .checkIfCARDIsDownloaded()
+    .checkCARDIdExists(cardId)
+
+    aroIndex <- read.csv(paste(get_cache_dir(get_pkg_info("ginmappeR")),'/card-data/aro_index.tsv', sep=''), sep='\t')
+    nucleotideId <- aroIndex[aroIndex$ARO.Accession == (if (grepl("^ARO:", cardId)) cardId else paste0("ARO:", cardId)),]$DNA.Accession
+    proteinId <- aroIndex[aroIndex$ARO.Accession == (if (grepl("^ARO:", cardId)) cardId else paste0("ARO:", cardId)),]$Protein.Accession
+
+    result <- getNCBIProtein2UniProt(proteinId, exhaustiveMapping = exhaustiveMapping, byIdenticalProteins = TRUE)
+    if(exhaustiveMapping | length(result)==0){
+        result <- unique(c(result, getNCBINucleotide2UniProt(nucleotideId, exhaustiveMapping = exhaustiveMapping, byIdenticalProteins = TRUE)))
+    }
+    return(result)
+}
+
+#########################
+# CARD database to KEGG #
+#########################
+
+### TODO: documentation:
+# Warn that byIdenticalProteins and bySimilarGenes combined with exhaustiveMapping takes a long time to finish
+getCARD2KEGG <- function(cardId, exhaustiveMapping = FALSE, detailedMapping = FALSE, byIdenticalProteins = TRUE, bySimilarGenes = TRUE){
+
+    .checkIfCARDIsDownloaded()
+    .checkCARDIdExists(cardId)
+
+    aroIndex <- read.csv(paste(get_cache_dir(get_pkg_info("ginmappeR")),'/card-data/aro_index.tsv', sep=''), sep='\t')
+    nucleotideId <- aroIndex[aroIndex$ARO.Accession == (if (grepl("^ARO:", cardId)) cardId else paste0("ARO:", cardId)),]$DNA.Accession
+    proteinId <- aroIndex[aroIndex$ARO.Accession == (if (grepl("^ARO:", cardId)) cardId else paste0("ARO:", cardId)),]$Protein.Accession
+
+    result <- getNCBIProtein2KEGG(proteinId, exhaustiveMapping = exhaustiveMapping, detailedMapping = detailedMapping, byIdenticalProteins = byIdenticalProteins, bySimilarGenes = bySimilarGenes)
+
+    if(exhaustiveMapping | length(result)==0){
+        aux <- getNCBINucleotide2KEGG(nucleotideId, exhaustiveMapping = exhaustiveMapping, detailedMapping = detailedMapping, byIdenticalProteins = byIdenticalProteins, bySimilarGenes = bySimilarGenes)
+        if(detailedMapping){
+            result <- lapply(.mergeNamedLists(result, aux), unique)
+        }else{
+            result <- unique(c(result, aux))
+        }
+    }
+    return(result)
+}
 
 
 
--- tests/CARDUnitTests.R
+++ tests/CARDUnitTests.R
@@ -7,6 +7,10 @@ library('httr')
 library('rentrez')
 library('XML')
 
+#########################
+# CARD database to NCBI #
+#########################
+
 ### Test getCARD2NCBIProtein
 
 # Positive cases
@@ -26,10 +30,47 @@ checkEquals(getCARD2NCBINucleotide('3003988'), 'KX531051.1')
 checkException(getCARD2NCBINucleotide('test'))
 
 ### Test getCARD2NCBIGene
-checkEquals(getCARD2NCBIGene('3002524'), '29426913')
-checkEquals(getCARD2NCBIGene('ARO:3002524'), '29426913')
+
+# Positive cases
+checkEquals(getCARD2NCBIGene('3002524'), c('29426913'))
+checkEquals(getCARD2NCBIGene('ARO:3002524'), c('29426913'))
 checkEquals(getCARD2NCBIGene('ARO:3002524', TRUE), c('29426913'))
-checkEquals(getCARD2NCBIGene('3002525'), '886648')
+checkEquals(getCARD2NCBIGene('3002525'), c('886648'))
 # No translation case
 checkEquals(getCARD2NCBIGene('3005061'), character(0))
+
+############################
+# CARD database to UniProt #
+############################
+
+### Test getCARD2UniProt
+
+# Positive cases
+checkEquals(getCARD2UniProt('3002867'), c('Q9ZIF9'))
+checkTrue(length(getCARD2UniProt('3002867', TRUE)) == 76)
+checkEquals(getCARD2UniProt('3003649'), c('A0A0K0TQH5'))
+# No translation case
+checkEquals(getCARD2UniProt('3006267'), character(0))
+
+#########################
+# CARD database to KEGG #
+#########################
+
+### Test getCARD2KEGG
+
+# Positive cases
+checkEquals(getCARD2KEGG('3000938'), c('ag:AAF19151'))
+checkEquals(getCARD2KEGG('3000938', detailedMapping = TRUE), list('DT'=c('ag:AAF19151')))
+checkEquals(getCARD2KEGG('3001109', detailedMapping = TRUE), list('0.9'=c('ag:BAA84973')))
+checkEquals(getCARD2KEGG('3002511', exhaustiveMapping = TRUE, detailedMapping = TRUE, bySimilarGenes = TRUE),
+            list('0.5'=c("chk:D4L85_28045","proe:H9L23_08075")))
+# No translation cases
+checkEquals(getCARD2KEGG('3006267', detailedMapping = FALSE, bySimilarGenes = FALSE), character(0))
+checkEquals(getCARD2KEGG('3006267', detailedMapping = TRUE, bySimilarGenes = FALSE), list())
+
+
+
+
+
+
 
