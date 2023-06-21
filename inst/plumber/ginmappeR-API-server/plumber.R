ce507d27af1b6c2fc9a990fcab9ce71abe3302ca
Fernando Sola <fersolesp@alum.us.es>
2023-06-21 10:57:35 +0200

    Version update

--- inst/plumber/ginmappeR-API-server/plumber.R
+++ inst/plumber/ginmappeR-API-server/plumber.R
@@ -24,6 +24,15 @@ errorHandler <- function(func, res, ...){
     })
 }
 
+# ------- DELETE
+#* Test endpoint
+#* @get /test
+function(){
+    return('Test endpoint')
+}
+#
+
+
 #########################################################
 #        Deactivated endpoints for server version       #
 #########################################################
